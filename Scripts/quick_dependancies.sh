7a0001afb76396658139050f33aa48f8ab876495
wql2002 <wqlthu2020@gmail.com>
2024-05-20 04:45:19 -0400

    feat: update env build scripts

--- Scripts/quick_dependancies.sh
+++ Scripts/quick_dependancies.sh
@@ -1,12 +1,23 @@
-sudo apt update && sudo apt upgrade -y
+sudo apt-get update && sudo aapt-getpt upgrade -y
 sudo apt-get install -y python
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 python3 -m pip install numpy
 #sudo python3-pip install numpy
-sudo add-apt-repository ppa:keithw/mahimahi -y
-sudo apt-get update
-sudo apt-get install -y mahimahi
+# sudo add-apt-repository ppa:keithw/mahimahi -y
+# sudo apt-get update
+# sudo apt-get install -y mahimahi
+sudo apt-get install -y protobuf-compiler libprotobuf-dev \
+    autotools-dev dh-autoreconf iptables pkg-config dnsmasq-base \
+    apache2-bin apache2-dev debhelper libssl-dev ssl-cert \
+    libxcb-present-dev libcairo2-dev libpango1.0-dev
+cd ~
+git clone https://github.com/ravinet/mahimahi
+cd mahimahi
+./autogen.sh
+./configure
+make
+sudo make install
 sudo dpkg-reconfigure -p critical dash
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo apt-get install -y libnetfilter-queue-dev
